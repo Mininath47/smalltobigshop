7496c5ecba9044521d7e92418888d2d958dd01b0
Mininath47 <meninathhonmane953@gmail.com>
2025-06-24 16:30:36 +0530

    hello

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
+app.use("/images", express.static("images"));
+
 
 const db = await MongoConnect();
 const demoCollection = db.collection("demo")
